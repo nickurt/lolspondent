1bc4a3953c2cb438201861e68946483d38615319
nickurt <nickurt@users.noreply.github.com>
2014-12-12 19:42:30 +0100

    Code intent fix  ..

--- public_html/index.php
+++ public_html/index.php
@@ -4,19 +4,17 @@ require __DIR__ . '/../vendor/autoload.php';
 define('TEMPLATE_PATH', 	__DIR__ .'/../templates');
 
 // Slim
-$app = new \Slim\Slim(
-	[
-    	'templates.path' 	=> 	TEMPLATE_PATH,
-    	'debug'				=> 	true,
-    	'log.enabled'		=>	false,
-    	'log.level'			=> 	\Slim\Log::DEBUG,
-    	'mode'				=>	'production',
- 	]
-);
+$app = new \Slim\Slim([
+	'templates.path'	=>	TEMPLATE_PATH,
+	'debug'				=>	true,
+	'log.enabled'		=>	false,
+	'log.level'			=>	\Slim\Log::DEBUG,
+	'mode'				=>	'production',
+]);
 
 // Singleton for the database
 $app->container->singleton('db', function () {
-    return new PDO('sqlite:'. realpath(__DIR__. '/../data/lolspondent.db'));
+	return new PDO('sqlite:'. realpath(__DIR__. '/../data/lolspondent.db'));
 });
 
 // Database
@@ -36,12 +34,12 @@ $app->get('/article', $article = function($id = 1) use ($db, $app) {
 
 	// Information about the paginator
 	$page_info		=	[
-							'all_pages' 	=>	$pages->rowCount(),
-							'current_page'	=> 	$id,
-							'max_pages'		=>	(int) floor($pages->rowCount()/15),	
-							'next_page'		=>	$id+1,
-							'prev_page'		=> 	$id-1,
-						];
+		'all_pages' 	=>	$pages->rowCount(),
+		'current_page'	=> 	$id,
+		'max_pages'		=>	(int) floor($pages->rowCount()/15),	
+		'next_page'		=>	$id+1,
+		'prev_page'		=> 	$id-1,
+	];
 
 	$articles 		= 	$db->prepare("SELECT *, COUNT( decorrespondent_id ) as count FROM posts GROUP BY decorrespondent_id ORDER BY decorrespondent_id desc LIMIT 15 OFFSET :offset");
 	$articles->bindValue(':offset', (int) ($page_info['current_page']-1) * 15, PDO::PARAM_INT);
@@ -55,8 +53,8 @@ $app->get('/article/page/:id', $article)->conditions(['id' => '[0-9]+']);
 
 // Show the Article
 $app->get('/article/:id', function ($id) use ($db, $app) {
-    $links 			= 	$db->prepare("SELECT * FROM posts WHERE decorrespondent_id = :id ORDER BY url ASC");
-    $links->bindValue(':id', (int) $id, PDO::PARAM_INT);
+	$links 			= 	$db->prepare("SELECT * FROM posts WHERE decorrespondent_id = :id ORDER BY url ASC");
+	$links->bindValue(':id', (int) $id, PDO::PARAM_INT);
 	$links->execute();
 	
     $app->render('articles/article.php', ['links' => $links], 200);
